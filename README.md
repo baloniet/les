2f0a0ca16c32cfbddabe8173782cb679605b9619
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2016-12-17 13:04:50 +0100

    rc1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # les
-Testing
+
---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash  
+echo "Preparing client"  
+cd ../bs4
+npm run meksis
+echo "Preparing server"  
+cd ../live_events_server
+npm run meksis 
+echo "Start git operations"
+cd ../les
+mv -v -f dist.tar.gz client.tgz
+mv -v -f live-events-server-1.0.0.tgz server.tgz
+git add -A && git commit -m $1
+git push
+echo $1 " compiled and commited. Run deploy on server."
+
+
