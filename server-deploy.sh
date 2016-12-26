77317506b5e64ee5651a1b2d29e12931fa464962
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2016-12-26 20:16:20 +0100

    RC2

--- server-deploy.sh
+++ server-deploy.sh
@@ -5,7 +5,7 @@ cp datasources.json package/server
 tar -czvf package.tgz package 
 slc deploy --service=live-events-server http://localhost:8701 ./package.tgz
 rm package.tgz
-rm -rf package
+rm -rf packagejanez
 rm -rf dist
 mkdir dist 
 tar -xzvf client.tgz dist
