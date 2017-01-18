28cbac6c8f79862f6134620ee898f03a5d0715ac
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-01-18 21:27:27 +0100

    RC4

--- server-deploy.sh
+++ server-deploy.sh
@@ -1,13 +1,13 @@
 rm -rf /package
 git pull
-tar -xzvf server.tgz
+tar -xzf server.tgz
 cp datasources.json package/server
-tar -czvf package.tgz package 
+tar -czf package.tgz package 
 slc deploy --service=live-events-server http://localhost:8701 ./package.tgz
 rm package.tgz
 rm -rf packagejanez
 rm -rf dist
 mkdir dist 
-tar -xzvf client.tgz dist
+tar -xzf client.tgz dist
 echo “Script completed.”
 pm2 show 0
