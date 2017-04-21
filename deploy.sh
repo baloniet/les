cc4a69e1d86d427698af2e38c4cea71c70ee9709
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-04-21 16:22:59 +0200

    RC8

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,14 @@
 echo "Preparing production client"  
 cd ../bs4
 npm run meksis
+
 echo "Preparing test client"  
 npm run meksis-test
+
+echo "Preparing test public"  
+cd ../live-events-public
+npm run meksis-test-public
+
 echo "Preparing server"  
 cd ../live_events_server
 npm run meksis 
@@ -11,6 +17,7 @@ echo "Start git operations"
 cd ../les
 mv -v -f dist.tar.gz client.tgz
 mv -v -f disttest.tar.gz client-test.tgz
+mv -v -f publicdisttest.tar.gz public-client-test.tgz
 mv -v -f live-events-server-1.0.0.tgz server.tgz
 git add -A && git commit -m $1
 git push
--- test-deploy.sh
+++ test-deploy.sh
@@ -7,8 +7,11 @@ slc deploy --service=live-events-server http://localhost:8701 ./package.tgz
 rm package.tgz
 rm -rf package
 rm -rf dist
+rm -rf publicdist
 mkdir dist 
 tar -xzf client-test.tgz dist
+mkdir publicdist 
+tar -xzf public-client-test.tgz publicdist
 echo “Script completed.”
 pm2 show 0
 slc ctl
