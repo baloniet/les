17dd855dbc2ca80c13b3dc9be7e5a36a5e1a2269
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-05-05 18:54:03 +0200

    RC8

--- test-deploy.sh
+++ test-deploy.sh
@@ -1,16 +1,16 @@
-echo "\E[1;32mgit pull\E[1;37m"
+echo -e "\E[1;32mgit pull\E[1;37m"
 git pull
 pm2 delete all
 cd dist
 
-echo "\E[1;32mpreparing test server\E[1;37m"
+echo -e "\E[1;32mpreparing test server\E[1;37m"
 tar -xzf server.tgz
 cp ../../datasources.json package/server
 tar -czf package.tgz package 
 slc deploy --service=live-events-server http://localhost:8701 ./package.tgz
 rm -rf package
 
-echo "\E[1;32mpreparing test register server\E[1;37m"
+echo -e "\E[1;32mpreparing test register server\E[1;37m"
 tar -xzf register.tgz
 rsync -av package/ register/
 cp ../../datasources.json register/app
@@ -19,13 +19,13 @@ pm2 start index.js --name register-server
 cd ..
 rm -rf package
 
-echo "\E[1;32mpreparing test client\E[1;37m"
+echo -e "\E[1;32mpreparing test client\E[1;37m"
 rm -rf client
 mkdir client
 tar -xzf client-test.tar.gz client
 pm2 start /usr/local/bin/http-server --name http-client -f -- client -p 8080
 
-echo "\E[1;32mpreparing test public\E[1;37m"
+echo -e "\E[1;32mpreparing test public\E[1;37m"
 rm -rf public
 mkdir public
 tar -xzf public-test.tar.gz public
@@ -35,6 +35,6 @@ rm *.tar.gz
 rm *.tgz
 cd ..
 
-echo “\E[1;32mScript completed.\E[1;37m"
+echo -e “\E[1;32mScript completed.\E[1;37m"
 slc ctl
 pm2 status
