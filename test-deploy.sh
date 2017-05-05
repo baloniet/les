085b489401b4064682dfc12f0d0835265601b603
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-05-05 18:43:23 +0200

    RC8

--- test-deploy.sh
+++ test-deploy.sh
@@ -1,17 +1,16 @@
-echo "git pull"
+echo "\E[1;32mgit pull\E[1;37m"
 git pull
 pm2 delete all
 cd dist
 
-echo "preparing test server"
+echo "\E[1;32mpreparing test server\E[1;37m"
 tar -xzf server.tgz
 cp ../../datasources.json package/server
 tar -czf package.tgz package 
 slc deploy --service=live-events-server http://localhost:8701 ./package.tgz
 rm -rf package
 
-echo "preparing test register server"
-#rm -rf register
+echo "\E[1;32mpreparing test register server\E[1;37m"
 tar -xzf register.tgz
 rsync -av package/ register/
 cp ../../datasources.json register/app
@@ -20,13 +19,13 @@ pm2 start index.js --name register-server
 cd ..
 rm -rf package
 
-echo "preparing test client"
+echo "\E[1;32mpreparing test client\E[1;37m"
 rm -rf client
 mkdir client
 tar -xzf client-test.tar.gz client
 pm2 start /usr/local/bin/http-server --name http-client -f -- client -p 8080
 
-echo "preparing test public"
+echo "\E[1;32mpreparing test public\E[1;37m"
 rm -rf public
 mkdir public
 tar -xzf public-test.tar.gz public
@@ -36,6 +35,6 @@ rm *.tar.gz
 rm *.tgz
 cd ..
 
-echo “Script completed.”
+echo “\E[1;32mScript completed.\E[1;37m"
 slc ctl
 pm2 status
