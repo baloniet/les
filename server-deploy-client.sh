0d802eee2a4ef44568c47904630a4755d466603d
janez.baloniet@gmail.com <janez.baloniet@gmail.com>
2017-05-09 20:43:15 +0200

    RC8

---
+++ server-deploy-client.sh
@@ -0,0 +1,18 @@
+echo -e "\E[1;32mgit pull\E[1;37m"
+git pull
+pm2 delete client
+cd dist
+
+echo -e "\E[1;32mpreparing production client\E[1;37m"
+rm -rf client
+mkdir client
+tar -xzf client.tar.gz client
+pm2 start /usr/bin/http-server --name http-client -f -- client -p 8080
+
+rm *.tar.gz
+rm *.tgz
+cd ..
+
+echo -e "\E[1;32mScript completed.\E[1;37m"
+slc ctl
+pm2 status
